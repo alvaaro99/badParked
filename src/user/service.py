98a2cc6c4ec6851f8ab46258cfcac0d1c118d864
Alvaro Acosta <alvaro99acosta@gmail.com>
2024-08-12 22:22:27 +0200

    user service created

---
+++ src/user/service.py
@@ -0,0 +1,39 @@
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import select
+from src.db.models import User
+from .schemas import *
+
+class UserService:
+
+    async def get_user(self, user_uid:str, session: AsyncSession):
+        query = select(User).where(User.uid == user_uid)
+
+        result = await session.exec(query)
+        return result.first()
+
+    async def create_user(self, user_data: UserCreateSchema, session: AsyncSession):
+        new_user = User(**user_data.model_dump())
+
+        session.add(new_user)
+        await session.commit()
+
+        return new_user
+
+    async def update_user(self, user_uid: str, user_data: UserUpdateSchema, session: AsyncSession):
+        user_to_update: User = await self.get_user(user_uid)
+
+        for key, value in user_data.model_dump().items():
+            setattr(user_to_update,key,value)
+
+        await session.commit()
+
+        return user_to_update
+    
+    async def delete_user(self, user_uid: str, session: AsyncSession):
+        user_to_delete = await self.get_user(user_uid, session)
+        if user_to_delete is not None:
+            await session.delete(user_to_delete)
+            await session.commit()
+
+            return 'Deleted'
+        return None
