37557a22d12acd4f1eea5fca8229a223a68b56a7
Alvaro Acosta <alvaro99acosta@gmail.com>
2024-08-27 16:20:05 +0200

    reuse response

--- src/user/service.py
+++ src/user/service.py
@@ -12,17 +12,8 @@ class UserService:
     async def register(self, user_data: UserCreateSchema, session: AsyncSession):
         user = await self.repository.create_user(user_data,session)
 
-        user_data = {'email':user.email,'fullname':f'{user.name} {user.surname}'}
-        access_token = create_token(user_data)
-        refresh_token = create_token(user_data,refresh=True)
+        return self.generate_data_token(user, 'Register Succesfull')
 
-        return JSONResponse(
-            content = {
-                'message':'Registered successfull',
-                'access_token':access_token,
-                'refresh_token':refresh_token
-            }
-        )
 
 
     async def login(self, user_data: UserLoginSchema, session: AsyncSession) -> User:
@@ -33,35 +24,30 @@ class UserService:
         if not verify_password(user_data.password,user.password):
             # TODO: raise Incorrect
             return None
+        
+        return self.generate_data_token(user, 'Login Succesfull')
+
+    
+
+    async def update(self, user_in_db: User, user_data: UserUpdateSchema, session: AsyncSession) -> User:
+        user_updated = await self.repository.update_user(user_in_db, user_data, session)
+
+        return self.generate_data_token(user_updated, 'Update Succesfull')
+    
+    async def delete(self, user: User, session: AsyncSession) -> bool:
+        #if not 
+        return await self.repository.delete_user(user.uid, session)
+            #TODO: raise exception
+
+    def generate_data_token(self, user: User, message: str = 'Ok'):
         user_data = {'email':user.email,'fullname':f'{user.name} {user.surname}'}
         access_token = create_token(user_data)
         refresh_token = create_token(user_data,refresh=True)
 
         return JSONResponse(
             content = {
-                'message':'Login successfull',
+                'message':message,
                 'access_token':access_token,
                 'refresh_token':refresh_token
             }
         )
-    
-
-    async def update(self, user_in_db: User, user_data: UserUpdateSchema, session: AsyncSession) -> User:
-        user_updated = await self.repository.update_user(user_in_db, user_data, session)
-
-        user_data = {'email':user_updated.email,'fullname':f'{user_updated.name} {user_updated.surname}'}
-        access_token = create_token(user_data)
-        refresh_token = create_token(user_data,refresh=True)
-
-        return JSONResponse(
-            content = {
-                'message':'Updated successfull',
-                'access_token':access_token,
-                'refresh_token':refresh_token
-            }
-        )
-    
-    async def delete(self, user: User, session: AsyncSession) -> bool:
-        #if not 
-        return await self.repository.delete_user(user.uid, session)
-            #TODO: raise exception
